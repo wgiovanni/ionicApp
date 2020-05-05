bf99fbf3c6c4661cd53e68c87a9469f410ecfa70
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2020-05-05 17:12:55 -0400

    Mostrar elementos seleccionados en la vista

--- src/app/pages/description-planets/description-planets.page.ts
+++ src/app/pages/description-planets/description-planets.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SwapiService } from 'src/app/servcices/swapi.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-description-planets',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./description-planets.page.scss'],
 })
 export class DescriptionPlanetsPage implements OnInit {
-
-  constructor() { }
+  planets:any;
+  constructor(
+    private swapiService:SwapiService,
+    private route:ActivatedRoute
+    ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      let id = params.get('id');
+      this.planets = this.swapiService.getSwapi('planets/' + id).subscribe(data=> {
+        this.planets = data;
+        console.log(data);
+      });
+    });
   }
+
+
 
 }
